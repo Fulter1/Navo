b11855466d2b2d01b40862e651608550e122c291
Fulter1 <sk7w22@gmail.com>
2026-05-14 09:21:35 +0300

    navo-config.js

--- navo-config.js
+++ navo-config.js
@@ -1,4 +1,4 @@
 window.NAVO_CLOUD = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://bsmivunyjbkowlgjwqyt.supabase.co/",
+  supabaseAnonKey: "sb_publishable_C9VWCoS0uF4v3Ujd5kB8kw_23SYEdZa"
 };
