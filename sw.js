c9a5033b8bf126420e2d03dad7a6f7d92d1322b5
Fulter1 <sk7w22@gmail.com>
2026-05-14 10:40:03 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-const CACHE='navo-x-cloud-v1';
-const FILES=['./','./index.html','./style.css','./app.js','./navo-config.js','./manifest.webmanifest','./assets/navo-icon.png','./assets/navo-mark.svg','./assets/navo-logo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = 'navo-v1.0.1';
+const ASSETS = [
+  './', './index.html', './style.css', './app.js', './navo-config.js',
+  './manifest.webmanifest', './assets/navo-mark.svg', './assets/navo-icon.png', './assets/navo-logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
