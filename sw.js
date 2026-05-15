82594be488a84a3007fb611ac116c63a040ae2f0
Fulter1 <sk7w22@gmail.com>
2026-05-15 14:53:06 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'navo-ultimate-v1';
-const ASSETS = ['./','./index.html','./style.css','./app.js','./navo-config.js','./manifest.webmanifest','./assets/navo-mark.svg','./assets/navo-icon.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{})); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { if (e.request.method !== 'GET') return; e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{}); return res; }).catch(()=>caches.match('./index.html')))); });
