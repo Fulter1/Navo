aaf19d3d8b65563d6a021a6c377a29e4d3a8123d
Fulter1 <sk7w22@gmail.com>
2026-05-15 11:28:33 +0300

    navo-config.js

--- navo-config.js
+++ navo-config.js
@@ -4,11 +4,10 @@
    3) Paste Project URL + anon public key below
 */
 window.NAVO_CLOUD = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://bsmivunyjbkowlgjwqyt.supabase.co",
+  supabaseAnonKey: "sb_publishable_C9VWCoS0uF4v3Ujd5kB8kw_23SYEdZa"
 };
 
 window.NAVO_CONFIG = window.NAVO_CONFIG || {
   SUPABASE_URL: window.NAVO_CLOUD.supabaseUrl,
   SUPABASE_ANON_KEY: window.NAVO_CLOUD.supabaseAnonKey
-};
